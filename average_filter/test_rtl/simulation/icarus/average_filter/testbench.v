ff757e5c451b345aa70d13cd0d1e5a044154ebee
fjpolo <fjpolo@gmail.com>
2025-03-12 17:50:23 +0100

    Update testbench

--- average_filter/test_rtl/simulation/icarus/average_filter/testbench.v
+++ average_filter/test_rtl/simulation/icarus/average_filter/testbench.v
@@ -58,11 +58,25 @@ wire o_ce;
     .data_in(data_in),
     .data_out(data_out),
     .o_ce(o_ce)
+`ifdef MCY
+    ,
+    .o_sum_ce(o_sum_ce),
+    .o_last_sample(o_last_sample),
+    .o_sum_ff(o_sum_ff)
+    `endif
   );
 `endif
 
 parameter CLK_PERIOD = 10;
 parameter TEST_VECTOR_SIZE = 10;
+
+`ifdef MCY
+  wire [0:0]               o_sum_ce;
+  wire [(DATA_WIDTH-1):0]  o_last_sample;
+  reg  signed [(DATA_WIDTH-1):0]  tb_last_sample;
+  wire [(DATA_WIDTH):0]    o_sum_ff;
+  reg  signed [(DATA_WIDTH):0]    tb_sum_ff;
+`endif
 
 reg [(DATA_WIDTH-1):0] test_vectors [0:TEST_VECTOR_SIZE-1];
 reg signed [(DATA_WIDTH-1):0] expected_outputs [0:TEST_VECTOR_SIZE-1];
@@ -117,6 +131,11 @@ initial begin
   reset_n = 1;
   if(o_ce) $display("FAIL: o_ce should be low after reset!\r\n");
   if(data_out != 'h00) $display("FAIL: data_out should be 'h00 after reset!\r\n");
+`ifdef MCY
+  if(o_sum_ce) $display("FAIL: o_sum_ce should be low after reset!\r\n");
+  if(o_last_sample != 'h00) $display("FAIL: o_last_sample should be low after reset!\r\n");
+  if(o_sum_ff != 'h00) $display("FAIL: o_sum_ff should be low after reset!\r\n");
+`endif
 
 
   // Apply the first test vector and wait 3 clock cycles.
@@ -125,14 +144,27 @@ initial begin
 
   // Now start the main test loop.
   data_in = test_vectors[0];
+  tb_last_sample = test_vectors[0];
   i_ce = 1;
   #CLK_PERIOD;
+`ifdef MCY
+  if(!o_sum_ce) $display("FAIL: sum_ce should be high after i_ce\r\n");
+`endif
   #CLK_PERIOD;
+  i_ce = 0;
+  if(!o_ce) $display("FAIL: o_ce should be high two clocks after i_ce\r\n");
   for (i = 1; i < TEST_VECTOR_SIZE-1; i = i + 1) begin
     data_in = test_vectors[i];
+    tb_last_sample = test_vectors[i-1];
     i_ce = 1;
     #CLK_PERIOD;
+    i_ce = 0;
+`ifdef MCY
+  if(!o_sum_ce) $display("FAIL: sum_ce should be high after i_ce\r\n");
+  if(o_sum_ff != (data_in + tb_last_sample)) $display("FAIL: Internal sum_ff failed. Expected: %d <-> current: %d\r\n", (data_in + tb_last_sample), o_sum_ff);
+`endif
     #CLK_PERIOD;
+    if(!o_ce) $display("FAIL: o_ce should be high two clocks after i_ce\r\n");
     if ((data_out !== expected_outputs[i])||(!o_ce)) begin
       $display("FAIL: Test Vector %d, Expected %d, Actual %d", i, expected_outputs[i], data_out);
       error_count = error_count + 1;
