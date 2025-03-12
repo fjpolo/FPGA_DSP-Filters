daa667ddb2d86537efdaaec435892d7fe863b725
fjpolo <fjpolo@gmail.com>
2025-03-12 21:13:33 +0100

    Add basic boxcar average filter

--- boxcar_filter/rtl/boxcar_filter.v
+++ boxcar_filter/rtl/boxcar_filter.v
@@ -25,23 +25,89 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 // =============================================================================
-
 `default_nettype none
 `timescale 1ps/1ps
 
-module boxcar_filter(
-    input   wire    [0:0]   i_clk,
-    input   wire    [0:0]   i_reset_n,
-    input   wire    [7:0]   i_data,
-    output  reg     [7:0]   o_data
+module boxcar_filter #(
+    parameter DATA_WIDTH = 8,
+    parameter NUM_SAMPLES = 8
+) (
+    input   wire    [0:0]               i_clk,
+    input   wire    [0:0]               i_reset_n,
+    input   wire    [0:0]               i_ce,
+    input   signed  [DATA_WIDTH-1:0]    i_data,
+    output  signed  [DATA_WIDTH-1:0]    o_data,
+    output  wire    [0:0]               o_ce
 );
 
-always @(posedge i_clk) begin
-    if(!i_reset_n) begin
-        o_data <= 8'h00;
-    end else begin
-        o_data <= i_data;
+    localparam INDEX_WIDTH = $clog2(NUM_SAMPLES);
+    localparam MAX_INDEX = NUM_SAMPLES - 1;
+
+    reg [INDEX_WIDTH-1:0] sample_index;
+    reg signed [(DATA_WIDTH + (INDEX_WIDTH-1)):0] accumulator;
+    integer i;
+
+    // Helper wires
+    wire sample_index_is_max;
+    /* verilator lint_off WIDTHEXPAND */
+    assign sample_index_is_max = (sample_index == MAX_INDEX);
+    /* verilator lint_on WIDTHEXPAND */
+
+    // valid_reg
+    reg valid_reg;
+    initial valid_reg = 1'b0;
+    always @(posedge i_clk) begin
+        if (!i_reset_n) begin
+            valid_reg <= 1'b0;
+        end else begin
+            if(i_ce) begin
+                if (sample_index_is_max)
+                    valid_reg <= 1'b1;
+                else
+                    valid_reg <= 1'b0;
+            end
+        end
     end
-end
+
+    // sample_index
+    initial sample_index = 'h00;
+    always @(posedge i_clk) begin
+        if (!i_reset_n) begin
+            sample_index <= 0;
+        end else begin
+            if(i_ce) begin
+                if (sample_index_is_max)
+                    sample_index <= 0;
+                else
+                    sample_index <= sample_index + 1;
+            end
+        end
+    end
+
+    // sample_buffer memory
+    reg signed [DATA_WIDTH-1:0] sample_buffer [NUM_SAMPLES-1:0];
+    initial begin
+        for (i = 0; i < NUM_SAMPLES; i = i + 1) begin
+            sample_buffer[i] = 0;
+        end
+    end
+    always @(posedge i_clk) begin
+        if(i_ce)
+            sample_buffer[sample_index] <= i_data;
+    end
+
+    // accummulator
+    always @(posedge i_clk) begin
+        if (!i_reset_n) begin
+            accumulator <= 0;
+        end else if (i_ce) begin
+            for (i = 0; i < NUM_SAMPLES; i = i + 1) begin
+                accumulator <= accumulator + { {INDEX_WIDTH{sample_buffer[i][DATA_WIDTH-1]}}, sample_buffer[i] };
+            end
+        end
+    end
+
+    assign o_data = accumulator[DATA_WIDTH + INDEX_WIDTH - 1:INDEX_WIDTH];
+    assign o_ce = valid_reg;
 
 endmodule
