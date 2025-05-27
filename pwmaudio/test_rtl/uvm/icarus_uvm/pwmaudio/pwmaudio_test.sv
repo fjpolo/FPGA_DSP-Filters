2565c78877e8e9bb8abe2e0e6a8753890864bc2c
fjpolo <fjpolo@gmail.com>
2025-05-27 22:16:32 +0200

    Dump PWMaudio project

---
+++ pwmaudio/test_rtl/uvm/icarus_uvm/pwmaudio/pwmaudio_test.sv
@@ -0,0 +1,20 @@
+class pwmaudio_test extends uvm_test;
+    `uvm_component_utils(pwmaudio_test)
+    
+    pwmaudio_env env;
+    
+    function new(string name, uvm_component parent);
+        super.new(name, parent);
+    endfunction
+    
+    virtual function void build_phase(uvm_phase phase);
+        super.build_phase(phase);
+        env = pwmaudio_env::type_id::create("env", this);
+    endfunction
+    
+    virtual task run_phase(uvm_phase phase);
+        phase.raise_objection(this);
+        #1000;
+        phase.drop_objection(this);
+    endtask
+endclass
